93b1acf1ed998e495d48c5ba41c13d6d3146eb53
David Colby <davidlcolby@gmail.com>
2014-05-23 13:06:12 -0500

    Checkin and checkout commands working

--- lib/commands/check_in_user.rb
+++ lib/commands/check_in_user.rb
@@ -1,9 +1,8 @@
 class MakerSpots::CheckinUser
 
   def self.run(user_id, loc_id)
-
-  	checkins = MakerSpots.db.get_checkins_by_user(user_id)
-  	MakerSpots.db.checkout(checkins.id)
+  	# checkins = MakerSpots.db.get_checkins_by_user(user_id)
+  	MakerSpots.db.checkout(user_id)
   	new_checkin = MakerSpots.db.create_checkin(location_id: loc_id, user_id: user_id)
   	{
   		success?: true,
--- lib/commands/checkout_user.rb
+++ lib/commands/checkout_user.rb
@@ -1,10 +1,10 @@
 class MakerSpots::CheckOutUser
 
   def self.run(user_id)
-
-    MakerSpots.db.checkout(user_id)
-    {
+    checked_out = MakerSpots.db.checkout(user_id)
+    result = {
       success?: true,
+      checked_out: checked_out,
       message: "Checked out successfully"
     }
   end

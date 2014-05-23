957d8fbb8f1f65a06a524b5cae9c8de8c77ae023
Benjamin Poon <benjamintpoon@gmail.com>
2014-05-23 11:34:33 -0500

    GetGravatar moved to its own command, called by sign_in_user, mod'ed tests for both

--- lib/commands/sign_in_user.rb
+++ lib/commands/sign_in_user.rb
@@ -1,25 +1,17 @@
-require 'digest/md5'
-
 class MakerSpots::SignInUser
 
   def self.run(email, password)
+
   	user = MakerSpots.db.get_user_by_email(email)
   	return { success?: false, error: "Email does not match our records" } if !user
   	return { success?: false, error: "Incorrect Password" } if password != user.password
-  	{
-  		success?: true,
-  		user: user,
-  		user_gravatar_img: self.get_gravatar_img(user.email),
-  		message: "#{user.name} signed in."
-  	}
+    result = {
+      success?: true,
+      user: user,
+      message: "#{user.name} signed in."
+    }
+    gravatar = MakerSpots::GetGravatar.run(email, {default: 'mm'})
+    gravatar[:success?] ? result.merge(gravatar) : result
   end
-
-  # more info: https://en.gravatar.com/site/implement/images/
-  # if opts[:default] is set, the image will not be resized, even with opts[:size!]
-	def self.get_gravatar_img(email, opts={})
-		hash = Digest::MD5.hexdigest(email.downcase)
-		url = "http://www.gravatar.com/avatar/#{hash}.#{opts[:filetype] || 'png'}"
-		url += "?s=#{opts[:size] || 80}"
-		url += "&d=#{opts[:default] || 'mm'}"
-	end
+  
 end
--- spec/commands/sign_in_user_spec.rb
+++ spec/commands/sign_in_user_spec.rb
@@ -17,19 +17,7 @@ describe 'SignInUser' do
       expect(result[:success?]).to eq true
       expect(result[:user]).to be_a(User)
       expect(result[:user].id).to eq @user.id
-    end
-  end
-
-  describe 'get_gravatar_img' do
-    it 'returns a gravatar img url' do
-      img_url = "http://www.gravatar.com/avatar/52bad6c2e5375f389955d89d7f559a7b.png?s=80&d=mm"
-      expect(MakerSpots::SignInUser.get_gravatar_img('benjamintpoon@gmail.com')).to eq(img_url)
-    end
-
-    it 'returns an img url if given a default' do
-      sloth = "http://www.thatcutesite.com/uploads/2009/09/baby_sloth_box.jpg"
-      expected = "http://www.gravatar.com/avatar/a86a735eb78567e14fa33aa8b7da9952.png?s=80&d=http://www.thatcutesite.com/uploads/2009/09/baby_sloth_box.jpg"
-      expect(MakerSpots::SignInUser.get_gravatar_img('pp@poopoo.com', default: sloth)).to eq(expected)
+      expect(result.has_key?(:gravatar)).to eq true
     end
   end
 
